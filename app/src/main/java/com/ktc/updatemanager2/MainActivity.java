7fb79a60e68d06acaa2bc6cccc37f5f2db7aaa69
fengjw <fengjwxx@163.com>
2018-05-28 15:35:58 +0800

    V2.0_180528_1  init change!

--- app/src/main/java/com/ktc/updatemanager2/MainActivity.java
+++ app/src/main/java/com/ktc/updatemanager2/MainActivity.java
@@ -1,7 +1,9 @@
-package com.fengjw.updatemanager2;
+package com.ktc.updatemanager2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.ktc.updatemanager.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/ktc/updatemanager2/util/LogUtil.java
@@ -0,0 +1,9 @@
+package com.ktc.updatemanager2.util;
+
+/**
+ * Created by fengjw on 2018/5/28.
+ */
+
+public class LogUtil {
+
+}
